caa27335bc4ff4cf809fbb34832df578d7b14930
DANIIL <danmetalist@gmail.com>
2022-06-23 13:29:05 +0600

    bags

--- energy/energy/settings.py
+++ energy/energy/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -48,7 +49,6 @@ CELERYBEAT_SCHEDULE = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 INSTALLED_APPS = [
     'django_celery_beat',
-    'django_celery_results',
     'rest_framework',
     'api',
     'modbus',
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
